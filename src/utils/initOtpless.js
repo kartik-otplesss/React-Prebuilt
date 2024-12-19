8a9a6d3eb18f6b425cb4684e9f946e6933f24603
ykartik21 <ykartik21@gmail.com>
2024-12-19 12:46:12 +0530

    first commit

---
+++ src/utils/initOtpless.js
@@ -0,0 +1,27 @@
+export const initOTPless = (callback) => {
+
+	const otplessInit = Reflect.get(window, 'otplessInit')
+
+	const loadScript = () => {
+		const isScriptLoaded = document.getElementById("otpless-sdk");
+		if(isScriptLoaded) return;
+
+		const script = document.createElement('script')
+		script.id = 'otpless-sdk'
+		script.type = 'text/javascript'
+		script.src = 'https://otpless.com/v3/auth.js'
+		// TODO: Add your app id
+		script.setAttribute('data-appid', '4ULZ8ZIE096DPDSX64B0')
+		document.head.appendChild(script)
+	}
+
+	otplessInit ? otplessInit() : loadScript()
+
+	Reflect.set(window, 'otpless', callback)
+}
+
+
+export const unloadScript = () => {
+	const script = document.getElementById('otpless-sdk')
+	script && script.remove()
+}
